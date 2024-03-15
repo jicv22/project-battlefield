ece3761e6b137139a8bd00396ec4d22090cec10d
Denn-DB <155107984+Denn-DB@users.noreply.github.com>
2024-03-14 22:53:49 -0600

    PR- Pb 172 create colossus 9 (#6)
    * Colossus9
    
    * Adding end of line to the file
    
    ---------
    
    Co-authored-by: Isaac Cambronero <110417253+jicv22@users.noreply.github.com>

---
+++ Source/ProjectBattlefield/Colossus9.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OffensiveCharacter.h"
+#include "Colossus9.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBATTLEFIELD_API AColossus9 : public AOffensiveCharacter
+{
+	GENERATED_BODY()
+
+	public:
+		AColossus9();
+	
+};
